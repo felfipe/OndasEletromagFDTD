90e8ad6a045915a57a49800c0cffeb53566861fd
Victor Amaral <victoramaral@usp.br>
2020-05-20 04:34:18 -0300

    mudado

--- ondas.py
+++ ondas.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
@@ -18,26 +19,20 @@ kmax = 100
 Ttotal = 10*l/Vp#3.70E-5
 deltaT = Ttotal/nmax
 deltaZ = l/kmax#Respeita a condição de estabilidade
-v = []
-i = []
+V = numpy.zeros((nmax,kmax), int)
+I = numpy.zeros((nmax,kmax), int)
 '''
 c1 = -(2*deltaT/(deltaT*deltaZ*R+2*deltaZ*L))
 c2 = (2*L-deltaT*R)/(2*L+deltaT*R)
 c3 = -(2*deltaT)/(deltaT*deltaZ*G+2*deltaZ*C)
 c4 = (2*C-deltaT*G)/(2*C-deltaT*G)
 '''
-vl = []
-il = []
-v0 = [] #v inicial para n = 0 representa i -1/2 para qualque k
-i0 = []#i inicial para n = 0 representa n 0 para qualquer k
-v0.append(Z0/(Zg + Z0)*Vg)
-i0.append(Vg/(Zg+Z0))
-for j in range(kmax-1):
-    v0.append(0)
-    i0.append(0)
-v.append(v0)
-i.append(i0)
-#função para animar
+
+# condições iniciais para o tempo
+V[0][0] = (Z0/(Zg + Z0)*Vg)
+I[0][0] = (Vg/(Zg+Z0))
+
+
 
 '''
 
@@ -48,24 +43,14 @@ A primeira tensão que calculamos é a tensão no tempo 1 para diferentes valore
 para isso precisamos da tensão no tempo 0(que devemos inserir no vetor v0) e da corrente no tempo
 1/2(calculada no passo anterior) para qualquer ponto da LT
  '''
-for n in range (nmax):
-    for k in range (kmax):
-        if(k == 0):
-            iaux = c1*(v[n][k] - v[n][k])+c2*i[n][k]#v[n][k-1] esta fora da linha de transmissao logo v = 0 ou ???
-            il.append(iaux)
-        else:
-            iaux = (c1*(v[n][k]-v[n][k-1])+c2*i[n][k])
-            il.append(iaux)
-    i.append(il)
-    for k in range (0,kmax):
-        if (k == kmax-1):
-            vl.append(0)
-        else:
-            vaux = c3*(i[n+1][k+1]-i[n+1][k])+c4*v[n][k]
-            vl.append(vaux)
-    v.append(vl) 
-    vl = []
-    il = []
+for n in range (1, nmax):
+    I[n][0] = c1*(v[n][k] - v[n][k])+c2*i[n][k]
+    for k in range (1, kmax):
+        I[n][k] = I[n-1][k] - deltaT/(L*deltaZ)*(V[n-1][k] - V[n-1][k-1])
+    for k in range (1,kmax):
+        V[n][k-1] = V[n-1][k-1] - deltaT/(C*deltaZ)(I[n][k]-I[n][k-1])
+    I[n][kmax-1] = 0
+
 plt.plot(range(kmax),v[900])
 plt.show()
 
