70e3c91ed38e1c4f4979eb226b650469f21fbb5a
Victor Amaral <victoramaral@usp.br>
2020-05-23 05:38:24 -0300

    falta organizar

--- ondas.py
+++ ondas.py
@@ -1,26 +1,28 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+import math
 
-R = 0#Linha sem perdas
-G = 0#Linha sem perdas
-Vp = 0.9*3.00E8
-L = 1.85E-7
-C = 7.41E-11
-l = 1000
+def animate(n):
+    voltage_plot.set_ydata(V[n])
+    current_plot.set_ydata(I[n])
+    return voltage_plot,
 Z0 = 50
+Vp = 0.9*3.00E8
+L = Z0/Vp
+C = 1/(Z0*Vp)
+l = 10
+
 Vg = 2
 Zg = 75
-Zl = 100
-nmax = 1000
+Zl = 50
 kmax = 100
-Ttotal = 10*l/Vp#3.70E-5
-deltaT = Ttotal/nmax
+Ttotal = 10.5*l/Vp#3.70E-5
 deltaZ = l/kmax#Respeita a condição de estabilidade
-V = numpy.zeros((nmax,kmax), int)
-I = numpy.zeros((nmax,kmax), int)
+deltaT = deltaZ/Vp*0.7
+nmax = int(Ttotal/deltaT)
+V = numpy.zeros((nmax,kmax))
+I = numpy.zeros((nmax,kmax))
 '''
 c1 = -(2*deltaT/(deltaT*deltaZ*R+2*deltaZ*L))
 c2 = (2*L-deltaT*R)/(2*L+deltaT*R)
@@ -29,32 +31,40 @@ c4 = (2*C-deltaT*G)/(2*C-deltaT*G)
 '''
 
 # condições iniciais para o tempo
-V[0][0] = (Z0/(Zg + Z0)*Vg)
-I[0][0] = (Vg/(Zg+Z0))
+V[0][0] = Z0*2/(Zg+Z0)
+I[0][0] = V[0][0]/Z0
+gamaS = 2*deltaT/(Zg*C*deltaZ)
+if Zl == 0:
+    gamaL = math.inf
+elif Zl == math.inf:
+    gamaL = 0
+else:
+    gamaL = 2*deltaT/(C*Zl*deltaZ)
+print(Vp)
+print(deltaZ/Vp)
+print(nmax)
 
 
 
-'''
-
-A primeira corrente que calculamos é a corrente no tempo 1/2 para diferentes valores de k
-para isso precisamos da corrente no tempo -1/2(que devemos inserir no vetor i0) e da tensão
-no tempo 0 para qualquer ponto da LT.
-A primeira tensão que calculamos é a tensão no tempo 1 para diferentes valores de k
-para isso precisamos da tensão no tempo 0(que devemos inserir no vetor v0) e da corrente no tempo
-1/2(calculada no passo anterior) para qualquer ponto da LT
- '''
 for n in range (1, nmax):
-    I[n][0] = c1*(v[n][k] - v[n][k])+c2*i[n][k]
-    for k in range (1, kmax):
-        I[n][k] = I[n-1][k] - deltaT/(L*deltaZ)*(V[n-1][k] - V[n-1][k-1])
-    for k in range (1,kmax):
-        V[n][k-1] = V[n-1][k-1] - deltaT/(C*deltaZ)(I[n][k]-I[n][k-1])
-    I[n][kmax-1] = 0
+    V[n][0] = (1- gamaS)*V[n-1][0] -2*I[n-1][0] + 2/Zg*2 # fonte 2
+    if Zl == 0:
+        V[n][kmax-1] = 0
+    else:
+        V[n][kmax-1] =(1 - gamaL)*V[n-1][kmax-1] + 2*I[n-1][kmax-2]
+    for k in range (1,kmax-1):
+        V[n][k] = V[n-1][k] - (I[n-1][k]-I[n-1][k-1])
+    for k in range (0, kmax-1):
+        I[n][k] = I[n-1][k] - (deltaT*deltaT)/(L*C*deltaZ*deltaZ)*(V[n][k+1] - V[n][k])#k+1 e k
 
-plt.plot(range(kmax),v[900])
+V = V*(deltaT/(C*deltaZ))
+figure, (voltage,current) = plt.subplots(2,1)
+voltage.grid(True)
+current.grid(True)
+
+voltage_plot, = voltage.plot(numpy.linspace(0,l,kmax), V[0])
+current_plot, = current.plot(numpy.linspace(0,l,kmax), I[0])
+
+animation = animation.FuncAnimation(figure, func = animate, frames=numpy.arange(0, nmax, (int)(kmax/100)), interval = 100, repeat = False)
 plt.show()
-
-'''
-Do jeito que esta o codigo i[1][0] contem a corrente no i tempo 1/2, k 0
-e v [1][0] possui a tensao no tempo 1, posicao 1/2
-'''
+    
