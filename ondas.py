ee1df4cba9069720bb9c604180b2bd99aca4686e
Victor Amaral <victoramaral@usp.br>
2020-05-23 17:57:42 -0300

    organizando

--- ondas.py
+++ ondas.py
@@ -7,33 +7,40 @@ def animate(n):
     voltage_plot.set_ydata(V[n])
     current_plot.set_ydata(I[n])
     return voltage_plot,
+
+
+def fonte_1(t):
+    if t < 0:
+        return 0
+    else:
+        return 2
+
 Z0 = 50
 Vp = 0.9*3.00E8
 L = Z0/Vp
 C = 1/(Z0*Vp)
-l = 10
-
-Vg = 2
-Zg = 75
-Zl = 50
+l = 1000
+Zg = 50
+Zl = 0
 kmax = 100
 Ttotal = 10.5*l/Vp#3.70E-5
 deltaZ = l/kmax#Respeita a condição de estabilidade
-deltaT = deltaZ/Vp*0.7
+deltaT = deltaZ/Vp*0.05
 nmax = int(Ttotal/deltaT)
 V = numpy.zeros((nmax,kmax))
 I = numpy.zeros((nmax,kmax))
-'''
-c1 = -(2*deltaT/(deltaT*deltaZ*R+2*deltaZ*L))
-c2 = (2*L-deltaT*R)/(2*L+deltaT*R)
-c3 = -(2*deltaT)/(deltaT*deltaZ*G+2*deltaZ*C)
-c4 = (2*C-deltaT*G)/(2*C-deltaT*G)
-'''
 
 # condições iniciais para o tempo
 V[0][0] = Z0*2/(Zg+Z0)
 I[0][0] = V[0][0]/Z0
-gamaS = 2*deltaT/(Zg*C*deltaZ)
+if Zg == 0:
+    gamaS = math.inf
+elif Zg == math.inf:
+    gamaS = 0
+else:
+    gamaS = 2*deltaT/(C*Zg*deltaZ)
+
+
 if Zl == 0:
     gamaL = math.inf
 elif Zl == math.inf:
@@ -47,11 +54,11 @@ print(nmax)
 
 
 for n in range (1, nmax):
-    V[n][0] = (1- gamaS)*V[n-1][0] -2*I[n-1][0] + 2/Zg*2 # fonte 2
+    V[n][0] = (1 - gamaS)*V[n-1][0] -2*I[n-1][0] + 2/Zg*fonte_1(n*deltaT)
     if Zl == 0:
         V[n][kmax-1] = 0
     else:
-        V[n][kmax-1] =(1 - gamaL)*V[n-1][kmax-1] + 2*I[n-1][kmax-2]
+        V[n][kmax-1] = (1 - gamaL)*V[n-1][kmax-1] + 2*I[n-1][kmax-2]
     for k in range (1,kmax-1):
         V[n][k] = V[n-1][k] - (I[n-1][k]-I[n-1][k-1])
     for k in range (0, kmax-1):
@@ -65,6 +72,6 @@ current.grid(True)
 voltage_plot, = voltage.plot(numpy.linspace(0,l,kmax), V[0])
 current_plot, = current.plot(numpy.linspace(0,l,kmax), I[0])
 
-animation = animation.FuncAnimation(figure, func = animate, frames=numpy.arange(0, nmax, (int)(kmax/100)), interval = 100, repeat = False)
+animation = animation.FuncAnimation(figure, func = animate, frames=numpy.arange(0, nmax, (int)(kmax/10)), interval = 100, repeat = False)
 plt.show()
     
